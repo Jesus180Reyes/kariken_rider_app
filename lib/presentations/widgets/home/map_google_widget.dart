b4dca919c6d345281b4d59ed3887d0ba1b350b51
Jesus Reyes <luisdejesus200122@gmail.com>
2023-04-28 13:01:51 -0600

    Feat: Map Style updated on both pages

--- lib/presentations/widgets/home/map_google_widget.dart
+++ lib/presentations/widgets/home/map_google_widget.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:rider_app/config/data/map_style.dart';
 
 class MapGoogleWidget extends StatelessWidget {
+  final double? height;
+  final double? margin;
   const MapGoogleWidget({
     super.key,
+    this.height = 200,
+    this.margin = 30,
   });
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(13.312160, -87.177323),
@@ -12,17 +19,20 @@ class MapGoogleWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(30),
+      margin: EdgeInsets.all(margin!),
       alignment: Alignment.center,
       decoration: BoxDecoration(
         color: const Color(0xffF1F1F1),
         borderRadius: BorderRadius.circular(20),
       ),
-      height: 200,
+      height: height,
       width: double.infinity,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: const GoogleMap(
+        child: GoogleMap(
+          onMapCreated: (GoogleMapController controller) {
+            controller.setMapStyle(json.encode(mapStyle));
+          },
           myLocationButtonEnabled: true,
           myLocationEnabled: true,
           compassEnabled: true,
